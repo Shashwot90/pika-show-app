14cb78a50a52ffeb341eaaa287981f6cbb3a5d39
unknown <shashwotpradhan@gmail.com>
2024-06-24 14:09:41 +0545

    first commit

---
+++ src/Layout/index.tsx
@@ -0,0 +1,15 @@
+import { Box } from "@mui/material";
+import { ReactNode } from "react";
+
+interface LayoutProps {
+    children: ReactNode
+}
+ 
+
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <Box></Box>>
+  )
+}
+
+export default Layout
