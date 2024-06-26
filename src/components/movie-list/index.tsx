cd66400b723caa8f19c45104e41e302e59b4bfed
unknown <shashwotpradhan@gmail.com>
2024-06-26 14:06:39 +0545

    movie list

---
+++ src/components/movie-list/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+interface MovieListProps {
+    recommendedList: any
+}
+const MovieList = ({ recommendList}: any) => {
+  return (
+    <div>MovieList</div>
+  )
+}
+
+export default MovieList
---
+++ src/components/movie-list/movieTrendList.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+interface MovieTrendListProps {
+    trendingList: any
+}
+const MovieTrendList = ({ trendingList}: any) => {
+  return (
+    <div>MovieTrendList</div>
+  )
+}
+
+export default MovieTrendList
